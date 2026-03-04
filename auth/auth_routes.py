d5382167b953e86ca269c44fc45cc9bfb254a9ab
alwantra <aldikurniawan.btc@gmail.com>
2026-03-04 20:47:16 +0700

    update dashboard

--- auth/auth_routes.py
+++ auth/auth_routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from flask_bcrypt import Bcrypt
+from flask_dance.contrib.google import google
+
 from auth.user_model import User
 
 auth_bp = Blueprint("auth", __name__)
 bcrypt = Bcrypt()
-
 
 # ================= REGISTER =================
 @auth_bp.route("/register", methods=["GET", "POST"])
@@ -68,6 +69,51 @@ def login():
         return redirect("/")
 
     return render_template("login.html")
+
+
+# ================= GOOGLE LOGIN =================
+@auth_bp.route("/google_login")
+def google_login():
+
+    if not google.authorized:
+        return redirect("/login/google")
+
+    resp = google.get("/oauth2/v2/userinfo")
+
+    email = resp.json()["email"]
+
+    user_data = User.find_by_username(email)
+
+    if not user_data:
+        User.create(email, "google_login")
+        user_data = User.find_by_username(email)
+
+    user_obj = User(user_data["id"], user_data["username"])
+    login_user(user_obj)
+
+    return redirect("/")
+
+
+# ================= TELEGRAM LOGIN =================
+@auth_bp.route("/telegram_login")
+def telegram_login():
+
+    username = request.args.get("username")
+
+    if not username:
+        flash("Telegram login gagal")
+        return redirect("/login")
+
+    user_data = User.find_by_username(username)
+
+    if not user_data:
+        User.create(username, "telegram_login")
+        user_data = User.find_by_username(username)
+
+    user_obj = User(user_data["id"], user_data["username"])
+    login_user(user_obj)
+
+    return redirect("/")
 
 
 # ================= LOGOUT =================
